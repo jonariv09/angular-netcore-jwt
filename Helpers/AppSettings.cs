f3a1670a2c8a677def0ff80c2aa5735653c94a62
Agustin <agustin.narvaez@contollo.net>
2021-07-13 22:58:29 -0600

    Init

---
+++ Helpers/AppSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace angular_netcore_jwt.Helpers
+{
+    public class AppSettings
+    {
+        public string Secret { get; set; }
+    }
+}
---
+++ Models/AuthenticateRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace angular_netcore_jwt.Models
+{
+    public class AuthenticateRequest
+    {
+        [Required] 
+        public string Username { get; set; }
+
+        [Required] public string Password { get; set; }
+    }
+}
